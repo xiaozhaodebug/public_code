5d09ca13b54aacfadfd908c8327aed3bf889c667
xiaozhaodebug <xiaozhaodebug@gmail.com>
2026-03-12 11:56:15 +0800

    chore: add .gitignore and remove build artifacts
    - Add comprehensive .gitignore for embedded C projects
    - Remove compiled binaries (.o, .elf, .hex, .map, etc.)
    - Remove IDE auto-generated files (Keil OBJ, LIST, etc.)
    - Keep repository clean with source files only

--- 03 CANFD_Origin_Release/Debug_FLASH/Sources/uds/uds_pending.d
+++
@@ -1,9 +0,0 @@
-Sources/uds/uds_pending.o: ../Sources/uds/uds_pending.c \
- ../Sources/uds/uds_pending.h ../Sources/uds/uds_config.h \
- ../Sources/uds/uds_types.h
-
-../Sources/uds/uds_pending.h:
-
-../Sources/uds/uds_config.h:
-
-../Sources/uds/uds_types.h:
--- 03 CANFD_Origin_Release/Debug_FLASH/Sources/uds/uds_session.d
+++
@@ -1,9 +0,0 @@
-Sources/uds/uds_session.o: ../Sources/uds/uds_session.c \
- ../Sources/uds/uds_session.h ../Sources/uds/uds_types.h \
- ../Sources/uds/uds_config.h
-
-../Sources/uds/uds_session.h:
-
-../Sources/uds/uds_types.h:
-
-../Sources/uds/uds_config.h:
